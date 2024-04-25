228455d7c5ae592b8e1bb1d482911b7a9ade0a3e
Vilela <guilhermevilela@hubert.com.br>
2024-04-25 00:29:02 -0300

    fix(analytics): import analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import React from "react";
 import { I18nextProvider } from "react-i18next";
 import { ThemeProvider } from "styled-components";
@@ -12,6 +13,7 @@ function App() {
   return (
     <React.StrictMode>
       <ThemeProvider theme={theme}>
+        <Analytics />
         <I18nextProvider i18n={i18nInstance}>
           <GlobalStyle />
           <Home />
